d7e2c87ace4baafb9a06dae18f6b740c9773c77e
Nicole Dicochea <dicocheanicole@gmail.com>
2022-10-29 12:03:12 -0700

    add solution

---
+++ volumeOfACuboid.js
@@ -0,0 +1,15 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // PREP 
+      
+      // P: length,  width, height
+      // R: length * width * height
+      // E: getVolumeOfCuboid(1, 2, 2) = 4
+      // P: return 
+      
+      return length * width * height
+    }
+}
