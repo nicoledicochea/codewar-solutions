ac7d6e14a5f779ed366bd7a459ed5b107e563782
Nicole Dicochea <dicocheanicole@gmail.com>
2022-11-12 20:22:10 -0800

    add solution

---
+++ function2SquaringAnArgument.js
@@ -0,0 +1,12 @@
+// Write a function that takes an argument and returns the square of it.
+
+// PREP
+// P: an integer
+// R: the integer sqaured
+// E: function square(3) = 9 
+    // sqaure(-4) = 16
+// P: return square of squared number
+
+function square(num) {
+    return num ** 2
+}
