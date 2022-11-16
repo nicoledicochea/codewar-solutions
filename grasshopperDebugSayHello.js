2468f087c0525da5469327b2e7741392e1a5aca2
Nicole Dicochea <dicocheanicole@gmail.com>
2022-11-15 21:10:31 -0800

    add solution

---
+++ grasshopperDebugSayHello.js
@@ -0,0 +1,21 @@
+// Debugging sayHello function
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+
+
+// original function
+    // function sayHello(name) {
+    //     return 'Hello'
+    // }
+
+
+    
+// debugged function
+function sayHello(name) {
+    return `Hello, ${name}`
+    }
