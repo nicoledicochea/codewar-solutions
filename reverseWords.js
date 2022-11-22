55053bb530fadce6b232bbc01e3ea12fba1b2222
Nicole Dicochea <dicocheanicole@gmail.com>
2022-11-21 18:01:29 -0800

    refractor solution

--- reverseWords.js
+++ reverseWords.js
@@ -21,3 +21,23 @@ function reverseWords(str) {
     
     return splitArr.join(' ')
 }
+
+
+
+// improve function formatting
+
+function reverseWords(str) {
+    // P: a string
+    // R: reverse each word in a string
+    // E: reverseWords("This is an example!") ==> "sihT si na !elpmaxe"
+    // P: 
+      // split each word at a space
+      // save new split array into a variable
+      // use map on the split array
+        // split the string and reverse and join it
+      // join the split array with a space
+    
+    return str.split(' ').map(element => {
+        return element.split('').reverse().join('')
+    }).join(' ')
+}
