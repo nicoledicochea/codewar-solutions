a22c06a3abc0683eb3ce232d9b164a169b1c4234
Nicole Dicochea <dicocheanicole@gmail.com>
2022-11-21 18:01:43 -0800

    refractor solution

--- reverseWords.js
+++ reverseWords.js
@@ -32,7 +32,6 @@ function reverseWords(str) {
     // E: reverseWords("This is an example!") ==> "sihT si na !elpmaxe"
     // P: 
       // split each word at a space
-      // save new split array into a variable
       // use map on the split array
         // split the string and reverse and join it
       // join the split array with a space
