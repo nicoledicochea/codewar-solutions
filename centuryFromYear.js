55831cf1925268044485cc39ad30a2f46ac96c9f
Nicole Dicochea <dicocheanicole@gmail.com>
2022-12-01 20:40:51 -0800

    add solution

--- centuryFromYear.js
+++ centuryFromYear.js
@@ -27,3 +27,11 @@ function century(year) {
       return yearRounded
     }
 }
+
+
+// REFRACTOR
+
+function century(year) {
+    return Math.ceil(year/100); 
+    // using ceiling method to round up to nearest century (100)
+ }
