7ec0e3126c321078cf76c1dced5bdad4ba67c04f
Nicole Dicochea <dicocheanicole@gmail.com>
2022-12-06 22:30:17 -0800

    add solution

---
+++ returnToSanity.js
@@ -0,0 +1,18 @@
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return
+//       results;
+// }
+
+
+fix spacing
+
+function mystery() {
+    let results = {
+      sanity: 'Hello'
+    }
+    return results
+}
