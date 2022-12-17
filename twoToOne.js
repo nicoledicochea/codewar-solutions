a91f7a4befaae0264e69c08a4abfe514a15a3677
Nicole Dicochea <dicocheanicole@gmail.com>
2022-12-16 17:45:15 -0800

    add solution

---
+++ twoToOne.js
@@ -0,0 +1,35 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+/*
+P: 2 strings of letters
+R: a sorted string of a to z using letters contained in a or b
+P: convert string to array. split
+check if array includes letter,
+if so, filter out
+sort through array
+*/
+
+function longest(s1, s2) {
+  
+    let arr = []
+    
+    let split = (s1 + s2).split('').forEach((element) => {
+      if (!arr.includes(element)) {
+        arr.push(element)
+      }
+    })
+  
+      
+    return arr
+      .sort()
+      .join('')
+
+}
