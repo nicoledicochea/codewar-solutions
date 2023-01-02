283a161e74a5a25167a7e3844882e5c60508ae5e
Nicole Dicochea <dicocheanicole@gmail.com>
2023-01-01 17:02:41 -0800

    add slution

---
+++ removeAnchorFromUrl.js
@@ -0,0 +1,28 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+
+
+/*
+P: a string, url
+R: remove everything after the '#' anchor tag
+E: "www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+P: split array
+map array
+  conditional
+    return all elements that don't include '#'
+return joined array
+*/
+
+function removeUrlAnchor(url){
+    let anchorIndex = url.split('').indexOf('#')
+    return url
+      .split('')
+      .map((element, index) => {
+        if (anchorIndex === -1 || index < anchorIndex) {
+          return element
+        }
+      }).join('')
+}
+
+
